ac507f9f0e1c5338f6e0a39f9a82294646941069
jaime <jaimerod26@gmail.com>
2025-02-19 20:55:04 -0500

    february 19th 2

---
+++ typscript/2025/February/19/19b.js
@@ -0,0 +1,10 @@
+var firstNonConsecutive = function (arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        if (arr[i] + 1 !== arr[i + 1]) {
+            return arr[i + 1];
+        }
+    }
+    return null;
+};
+var nums = [1, 2, 3, 4, 6, 7, 8];
+console.log(firstNonConsecutive(nums));
